278d5c59b6222676c8a1e9792a64c5b2787b6cc3
micha <mimagra@gmx.net>
2022-10-29 00:30:43 +0200

    build connection to the DB via JPA and added an Account Table and a GetMapping to print out the content of the account Table (for zu Visualisierungszwecken)

---
+++ backend/src/main/java/de/dontletyopudie/backend/account/Account.java
@@ -0,0 +1,56 @@
+package de.dontletyopudie.backend.account;
+
+import javax.persistence.*;
+
+@Entity(name = "Account")
+@Table(name = "account", uniqueConstraints = {/*@UniqueConstraint(name = "account_email_unique", columnNames = "email"),*/
+        @UniqueConstraint(name = "account_user_name_unique", columnNames = "username")})
+public class Account {
+
+    @Id
+    @SequenceGenerator(name = "account_sequence", sequenceName = "account_sequence",allocationSize = 1)
+    @GeneratedValue(strategy = GenerationType.SEQUENCE, generator = "account_sequence")
+    @Column(name = "id", updatable = false)
+    private Long id;
+    @Column(name = "username", nullable = false)
+    private String username;
+    @Column(name = "password", nullable = false)
+    private String password;
+    @Column(name = "email", nullable = false)
+    private String email;
+
+    private Account() {
+    }
+
+    public Account(String username, String password, String email) {
+        this.username = username;
+        this.password = password;
+        this.email = email;
+    }
+
+    public Long getId() {
+        return id;
+    }
+
+    public String getUsername() {
+        return username;
+    }
+
+    public String getPassword() {
+        return password;
+    }
+
+    public String getEmail() {
+        return email;
+    }
+
+    @Override
+    public String toString() {
+        return "Account{" +
+                "id=" + id +
+                ", username='" + username + '\'' +
+                ", password='" + password + '\'' +
+                ", email='" + email + '\'' +
+                '}';
+    }
+}
