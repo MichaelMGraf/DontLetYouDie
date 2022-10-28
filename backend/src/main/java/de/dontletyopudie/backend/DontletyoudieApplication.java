7b47ee1e52e3ce259581659a592019833486a1ad
micha <mimagra@gmx.net>
2022-10-29 00:03:36 +0200

    Initialized Spring Boot with dependencies for the DB connection and spring security.

---
+++ backend/src/main/java/de/dontletyopudie/backend/DontletyoudieApplication.java
@@ -0,0 +1,18 @@
+package de.dontletyopudie.backend;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.CommandLineRunner;
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.Bean;
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@SpringBootApplication
+public class DontletyoudieApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(DontletyoudieApplication.class, args);
+	}
+}
