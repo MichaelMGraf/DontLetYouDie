4886a43de56bcb368a86934d8e76301b8d20dd78
glori <gloria.landsberger@gmail.com>
2022-11-08 15:12:12 +0100

    Added cucumber steps for accounts and proofs

--- backend/src/test/java/de/dontletyoudie/backend/CucumberTestRunner.java
+++ backend/src/test/java/de/dontletyoudie/backend/CucumberTestRunner.java
@@ -6,7 +6,7 @@ import org.junit.runner.RunWith;
 
 @RunWith(Cucumber.class)
 @CucumberOptions(
-        features = {"/src/test/resources/features"},
+        features = {"./src/test/resources/features"},
         plugin = {"pretty"},
         glue = {"de.dontletyoudie.backend.cucumberglue"})
 public class CucumberTestRunner {
---
+++ backend/src/test/java/de/dontletyoudie/backend/cucumberglue/Proofs.java
@@ -0,0 +1,23 @@
+package de.dontletyoudie.backend.cucumberglue;
+
+import io.cucumber.java.en.And;
+import io.cucumber.java.en.Then;
+import io.cucumber.java.en.When;
+import org.assertj.core.api.Assertions;
+
+public class Proofs {
+    @When("the user sends a request to {string}")
+    public void theUserSendsARequestTo(String arg0) {
+        // add code here
+    }
+
+    @Then("a picture with a unique name was saved to the filesystem")
+    public void aPictureWithAUniqueNameWasSavedToTheFilesystem() {
+        // add code here
+    }
+
+    @And("there exists a record with the corresponding filename in the database")
+    public void thereExistsARecordWithTheCorrespondingFilenameInTheDatabase() {
+        // add code here
+    }
+}
