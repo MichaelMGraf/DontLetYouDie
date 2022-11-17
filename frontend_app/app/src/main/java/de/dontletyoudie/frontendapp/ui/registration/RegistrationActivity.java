273a3a4d786a8813f30ec5c449d91408529cd549
Illumin80 <passrock0305@gmail.com>
2022-11-17 14:21:30 +0100

    added check for username and email format

--- frontend_app/app/src/main/java/de/dontletyoudie/frontendapp/ui/registration/RegistrationActivity.java
+++ frontend_app/app/src/main/java/de/dontletyoudie/frontendapp/ui/registration/RegistrationActivity.java
@@ -7,6 +7,7 @@ import android.os.Bundle;
 import android.view.View;
 import android.widget.Button;
 import android.widget.TextView;
+import android.widget.Toast;
 
 import de.dontletyoudie.frontendapp.R;
 import de.dontletyoudie.frontendapp.databinding.ActivityLoginBinding;
@@ -46,6 +47,30 @@ public class RegistrationActivity extends AppCompatActivity {
                 String email = emailEditText.getText().toString();
                 String password1 = passwordEditText.getText().toString();
                 String password2 = password2EditText.getText().toString();
+
+                //check username
+                //TODO check if username is available
+                if(username.equals("")) {
+                    showMessage("username is not allowed to be blank!");
+                    return;
+                }
+
+                //check email
+                String emailRegex = "(?:[a-z0-9!#$%&'*+/=?^_`{|}~-]+(?:\\.[a-z0-9!#$%&'*+/=?^_`{|}~-]+)*|\"(?:[\\x01-\\x08\\x0b\\x0c\\x0e-\\x1f\\x21\\x23-\\x5b\\x5d-\\x7f]|\\\\[\\x01-\\x09\\x0b\\x0c\\x0e-\\x7f])*\")@(?:(?:[a-z0-9](?:[a-z0-9-]*[a-z0-9])?\\.)+[a-z0-9](?:[a-z0-9-]*[a-z0-9])?|\\[(?:(?:25[0-5]|2[0-4][0-9]|[01]?[0-9][0-9]?)\\.){3}(?:25[0-5]|2[0-4][0-9]|[01]?[0-9][0-9]?|[a-z0-9-]*[a-z0-9]:(?:[\\x01-\\x08\\x0b\\x0c\\x0e-\\x1f\\x21-\\x5a\\x53-\\x7f]|\\\\[\\x01-\\x09\\x0b\\x0c\\x0e-\\x7f])+)\\])";
+                    // got this regex-String from https://emailregex.com/
+                if(email.equals("")) {
+                    showMessage("email ist not allowed to be blank!");
+                    return;
+                }else if(!email.matches(emailRegex)) {
+                    showMessage("invalid email!");
+                    return;
+                }
+
+                //TODO check password
+
+                //TODO check if both passwords are the same
+
+                //TODO check the form
                 //TODO now log in with this credentials
             }
         });
@@ -60,7 +85,9 @@ public class RegistrationActivity extends AppCompatActivity {
                 finish();
             }
         });
+    }
 
-
+    private void showMessage(String message) {
+        Toast.makeText(getApplicationContext(), message, Toast.LENGTH_LONG).show();
     }
 }
