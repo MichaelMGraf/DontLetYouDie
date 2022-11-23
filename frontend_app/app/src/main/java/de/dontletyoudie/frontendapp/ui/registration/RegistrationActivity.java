f66d097be4fe2d3c17500372e19dc8efa1e001a9
Illumin80 <passrock0305@gmail.com>
2022-11-23 23:19:22 +0100

    integrated apache common validator for email validation

--- frontend_app/app/src/main/java/de/dontletyoudie/frontendapp/ui/registration/RegistrationActivity.java
+++ frontend_app/app/src/main/java/de/dontletyoudie/frontendapp/ui/registration/RegistrationActivity.java
@@ -16,6 +16,7 @@ import de.dontletyoudie.frontendapp.R;
 import de.dontletyoudie.frontendapp.databinding.ActivityLoginBinding;
 import de.dontletyoudie.frontendapp.ui.login.LoginViewModel;
 import de.dontletyoudie.frontendapp.ui.login.LoginViewModelFactory;
+import org.apache.commons.validator.routines.EmailValidator;
 
 public class RegistrationActivity extends AppCompatActivity {
 
@@ -59,13 +60,12 @@ public class RegistrationActivity extends AppCompatActivity {
                 }
 
                 //check email
-                String emailRegex = "(?:[a-z0-9!#$%&'*+/=?^_`{|}~-]+(?:\\.[a-z0-9!#$%&'*+/=?^_`{|}~-]+)*|\"(?:[\\x01-\\x08\\x0b\\x0c\\x0e-\\x1f\\x21\\x23-\\x5b\\x5d-\\x7f]|\\\\[\\x01-\\x09\\x0b\\x0c\\x0e-\\x7f])*\")@(?:(?:[a-z0-9](?:[a-z0-9-]*[a-z0-9])?\\.)+[a-z0-9](?:[a-z0-9-]*[a-z0-9])?|\\[(?:(?:25[0-5]|2[0-4][0-9]|[01]?[0-9][0-9]?)\\.){3}(?:25[0-5]|2[0-4][0-9]|[01]?[0-9][0-9]?|[a-z0-9-]*[a-z0-9]:(?:[\\x01-\\x08\\x0b\\x0c\\x0e-\\x1f\\x21-\\x5a\\x53-\\x7f]|\\\\[\\x01-\\x09\\x0b\\x0c\\x0e-\\x7f])+)\\])";
-                    // got this regex-String from https://emailregex.com/
-                if(email.equals("")) {
-                    showMessage("email ist not allowed to be blank!");
+                EmailValidator validator = EmailValidator.getInstance();
+                 if (email.equals("")){
+                    showMessage("your email ist not allowed to be blank!");
                     return;
-                }else if(!email.matches(emailRegex)) {
-                    showMessage("invalid email!");
+                } else if (!validator.isValid(email)) {
+                    showMessage("your email is invalid");
                     return;
                 }
 
