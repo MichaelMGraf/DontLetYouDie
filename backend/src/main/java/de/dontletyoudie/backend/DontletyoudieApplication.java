767e9ddb37840226ceaccd340aaf269b2920e1d0
Illumin80 <passrock0305@gmail.com>
2022-11-30 19:49:53 +0100

    altered the default debug account values

--- backend/src/main/java/de/dontletyoudie/backend/DontletyoudieApplication.java
+++ backend/src/main/java/de/dontletyoudie/backend/DontletyoudieApplication.java
@@ -1,18 +1,30 @@
 package de.dontletyoudie.backend;
 
+import de.dontletyoudie.backend.persistence.account.Account;
+import de.dontletyoudie.backend.persistence.account.AccountRepository;
 import de.dontletyoudie.backend.persistence.account.AccountService;
 import de.dontletyoudie.backend.persistence.account.dtos.AccountAddDTO;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.CommandLineRunner;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.context.annotation.Bean;
 import org.springframework.dao.DataIntegrityViolationException;
+import org.springframework.web.bind.annotation.RestController;
 
 @SpringBootApplication
 public class DontletyoudieApplication {
+
+	/*
+	@Autowired
+	private AccountRepository repo;
+	*/
+
 	public static void main(String[] args) {
 		SpringApplication.run(DontletyoudieApplication.class, args);
 	}
+
+
 	@Bean
 	CommandLineRunner commandLineRunner (AccountService accountService) {
 		return args -> {
