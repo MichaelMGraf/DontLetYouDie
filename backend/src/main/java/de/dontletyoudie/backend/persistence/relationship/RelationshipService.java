770af031bcab34acf4b634c1bd80fc3dd11cc4c2
blumentopfy <81259038+blumentopfy@users.noreply.github.com>
2022-12-11 15:24:12 +0100

    DTLYD-72 RelationshipService now uses AccountService instead of AccountRepository Return HTTP.CREATED instead of OK after successfully adding new Relationship

--- backend/src/main/java/de/dontletyoudie/backend/persistence/relationship/RelationshipService.java
+++ backend/src/main/java/de/dontletyoudie/backend/persistence/relationship/RelationshipService.java
@@ -2,6 +2,7 @@ package de.dontletyoudie.backend.persistence.relationship;
 
 import de.dontletyoudie.backend.persistence.account.Account;
 import de.dontletyoudie.backend.persistence.account.AccountRepository;
+import de.dontletyoudie.backend.persistence.account.AccountService;
 import de.dontletyoudie.backend.persistence.relationship.dtos.RelationshipAddDto;
 import lombok.RequiredArgsConstructor;
 import org.springframework.security.core.userdetails.UsernameNotFoundException;
@@ -13,26 +14,22 @@ import java.util.Optional;
 @RequiredArgsConstructor
 public class RelationshipService {
 
-    private final AccountRepository accountRepository;
+    private final AccountService accountService;
     private final RelationshipRepository relationshipRepository;
 
     public Relationship save(RelationshipAddDto relationshipAddDto) {
-        Optional<Account> srcAccount = accountRepository.findAccountByUsername(relationshipAddDto.getSrcUsername());
-        Optional<Account> relAccount = accountRepository.findAccountByUsername(relationshipAddDto.getRelUsername());
-
-        if (srcAccount.isEmpty() || relAccount.isEmpty()) {
-            throw new UsernameNotFoundException("Couldn't find srcAccount or relAccount");
-        }
+        Account srcAccount = accountService.getAccount(relationshipAddDto.getSrcUsername());
+        Account relAccount = accountService.getAccount(relationshipAddDto.getRelUsername());
 
         Relationship returnRelationship = new Relationship();
 
-        if (relationshipRepository.findRelationshipBySrcAccountAndRelAccount(srcAccount.get(), relAccount.get()).isPresent()) {
+        if (relationshipRepository.findRelationshipBySrcAccountAndRelAccount(srcAccount, relAccount).isPresent()) {
             //TODO Behavior is a bit weird atm, but afaik we want to remodel adding friends anyways to have a proper search function, then this case will never happen
             return returnRelationship;
         } else {
             return relationshipRepository.save(new Relationship(
-                    srcAccount.get(),
-                    relAccount.get(),
+                    srcAccount,
+                    relAccount,
                     RelationshipStatus.PENDING
             ));
         }
