6f77a6737cbbb3976f697bff22f1cf87deb8dffa
micha <mimagra@gmx.net>
2023-04-13 21:32:28 +0200

    DTLYD-101 filter request through the methods with the filter annotation and removed the old way for filtering specific paths

--- backend/src/main/java/de/dontletyoudie/backend/security/filter/DDAuthorizationFilter.java
+++ backend/src/main/java/de/dontletyoudie/backend/security/filter/DDAuthorizationFilter.java
@@ -5,6 +5,7 @@ import com.fasterxml.jackson.databind.ObjectMapper;
 import de.dontletyoudie.backend.security.tokenservice.TokenService;
 import de.dontletyoudie.backend.security.tokenservice.TokenServiceFactory;
 import lombok.extern.slf4j.Slf4j;
+import org.springframework.data.util.AnnotatedTypeScanner;
 import org.springframework.http.HttpHeaders;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.MediaType;
@@ -19,25 +20,57 @@ import javax.servlet.ServletException;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
 import java.io.IOException;
+import java.lang.reflect.Method;
 import java.util.*;
 
 @Slf4j
 public class DDAuthorizationFilter extends OncePerRequestFilter {
 
     private final TokenService tokenService;
+    private final Map<String, Method> filterMethods = new HashMap<>();
+    private final Map<String, Method> filterMethodsWithToken = new HashMap<>();
 
     public DDAuthorizationFilter() {
         this.tokenService = TokenServiceFactory.getTokeService();
+        findFilter();
     }
+
+    private void findFilter() {
+        AnnotatedTypeScanner scanner = new AnnotatedTypeScanner(Filter.class);
+
+        Set<Class<?>> types = scanner.findTypes("de.dontletyoudie.backend");
+
+        for (Class<?> c : types) {
+            System.out.println(c.getName());
+            Class<?> c1;
+            try {
+                c1 = Class.forName(c.getName());
+            } catch (ClassNotFoundException e) {
+                continue;
+            }
+
+            for (Method m : c1.getDeclaredMethods()) {
+                if (! m.isAnnotationPresent(PathFilter.class)) continue;
+
+                String[] path = m.getAnnotation(PathFilter.class).path();
+                Map<String, Method> map =
+                        m.getAnnotation(PathFilter.class).tokenRequired() ? filterMethodsWithToken : filterMethods;
+
+                Arrays.stream(path).forEach(p -> {
+                    if (map.containsKey(p))
+                        throw new IllegalStateException("Multiple @PathFilter with same path and tokenRequired");
+                    map.put(p, m);
+                });
+
+
+            }
+        }
+    }
+
 
     @Override
     protected void doFilterInternal(HttpServletRequest request, HttpServletResponse response, FilterChain filterChain) throws ServletException, IOException {
-        if (request.getServletPath().equals("/login/token/refresh")) {
-            if (tryDecodeToken(request, response).isPresent()) filterChain.doFilter(request, response);
-            return;
-        }
-
-        if (request.getServletPath().equals("/login")) {
+        if (pathFilter(filterMethods, request, null)) {
             filterChain.doFilter(request, response);
             return;
         }
@@ -47,6 +80,11 @@ public class DDAuthorizationFilter extends OncePerRequestFilter {
             return;
         }
         DecodedJWT decodedToken = decodedTokenOptional.get();
+
+        if (pathFilter(filterMethodsWithToken, request, decodedToken)) {
+            filterChain.doFilter(request, response);
+            return;
+        }
 
         List<String> claims = decodedToken.getClaim("roles").asList(String.class);
         if (claims == null) {
@@ -65,6 +103,19 @@ public class DDAuthorizationFilter extends OncePerRequestFilter {
         } catch (Exception e) {
             respondError(response, e.getMessage(), HttpStatus.INTERNAL_SERVER_ERROR);
         }
+    }
+    private boolean pathFilter(Map<String, Method> filters, HttpServletRequest request, DecodedJWT token) {
+        boolean r = false;
+        if (filters.containsKey(request.getServletPath())) {
+            try {
+                r = (boolean) (token == null ?
+                        filters.get(request.getServletPath()).invoke(null, request)
+                        : filters.get(request.getServletPath()).invoke(null, request, token));
+            } catch (Exception e) {
+                r = true;
+            }
+        }
+        return r;
     }
 
     private Optional<DecodedJWT> tryDecodeToken(HttpServletRequest request, HttpServletResponse response)
