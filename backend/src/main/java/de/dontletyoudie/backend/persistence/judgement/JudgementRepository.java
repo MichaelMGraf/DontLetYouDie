77fe75229d3934cb02f8fdb288856acedbe938c1
blumentopfy <81259038+blumentopfy@users.noreply.github.com>
2023-04-30 15:51:58 +0200

    DTLYD-127 Create route for retrieving proofs to be judged by users

--- backend/src/main/java/de/dontletyoudie/backend/persistence/judgement/JudgementRepository.java
+++ backend/src/main/java/de/dontletyoudie/backend/persistence/judgement/JudgementRepository.java
@@ -2,5 +2,8 @@ package de.dontletyoudie.backend.persistence.judgement;
 
 import org.springframework.data.jpa.repository.JpaRepository;
 
+import java.util.List;
+
 public interface JudgementRepository extends JpaRepository<Judgement, Long> {
+    List<Judgement> findJudgementsByJudgeName(String judgeName);
 }
--- backend/src/main/java/de/dontletyoudie/backend/persistence/proof/ProofService.java
+++ backend/src/main/java/de/dontletyoudie/backend/persistence/proof/ProofService.java
@@ -1,40 +1,82 @@
 package de.dontletyoudie.backend.persistence.proof;
 
+import de.dontletyoudie.backend.persistence.account.Account;
+import de.dontletyoudie.backend.persistence.account.AccountRepository;
+import de.dontletyoudie.backend.persistence.judgement.Judgement;
+import de.dontletyoudie.backend.persistence.judgement.JudgementRepository;
 import de.dontletyoudie.backend.persistence.proof.dtos.ProofAddDto;
 import de.dontletyoudie.backend.persistence.proof.dtos.ProofReturnDto;
+import de.dontletyoudie.backend.persistence.relationship.Relationship;
+import de.dontletyoudie.backend.persistence.relationship.RelationshipRepository;
+import de.dontletyoudie.backend.persistence.relationship.RelationshipStatus;
 import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Service;
 
 import java.util.ArrayList;
 import java.util.List;
+import java.util.Optional;
 
 @Service("proofService")
 @RequiredArgsConstructor
 public class ProofService {
 
     private final ProofRepository proofRepository;
+    private final RelationshipRepository relationshipRepository;
+    private final AccountRepository accountRepository;
+    private final JudgementRepository judgementRepository;
 
-    public List<ProofReturnDto> getPendingProofs(String username) {
 
-        List<Proof> proofs = proofRepository.findProofsByUsername(username);
-        List<ProofReturnDto> proofReturnDto = new ArrayList<>();
+    public Optional<ProofReturnDto> getPendingProofs(String username) {
 
-        if (proofs.isEmpty()) {
-            return proofReturnDto;
+
+        //Get Account
+        Optional<Account> userAccount = accountRepository.findAccountByUsername(username);
+
+        // Find Relationships
+        Optional<List<Relationship>> relationships = relationshipRepository.findRelationshipsByRelAccountOrSrcAccount(userAccount.get(), userAccount.get());
+        List<Account> friends = new ArrayList<>();
+
+        if (relationships.isEmpty()) {
+            return Optional.empty();
         } else {
-            for (Proof proof : proofs) {
-                proofReturnDto.add(new ProofReturnDto(
-                        proof.getUsername(),
-                        proof.getImage(),
-                        proof.getDateTime(),
-                        proof.getCategory(),
-                        proof.getComment(),
-                        proof.getAvgScore(),
-                        proof.getJudgements()
-                ));
+            for (Relationship relationship : relationships.get()) {
+                if (relationship.getRelationshipStatus() == RelationshipStatus.FRIEND) {
+
+                    String srcUserName = relationship.getSrcAccount().getUsername();
+                    Account friend = srcUserName.equals(username) ? relationship.getRelAccount() : relationship.getSrcAccount();
+
+                    friends.add(friend);
+                }
             }
-            return proofReturnDto;
         }
+
+        // Gather Proofs the user has judged
+        List<Judgement> judgements = judgementRepository.findJudgementsByJudgeName(username);
+        List<Long> judgedProofIds = new ArrayList<>();
+
+        for (Judgement judgement : judgements) {
+            judgedProofIds.add(judgement.getProofId());
+        }
+
+
+        for (Account friend : friends) {
+            List<Proof> proofs = proofRepository.findProofsByUsername(friend.getUsername());
+            for (Proof proof : proofs) {
+                if (!judgedProofIds.contains(proof.getId())) {
+                    return Optional.of(new ProofReturnDto(
+                            proof.getUsername(),
+                            proof.getImage(),
+                            proof.getDateTime(),
+                            proof.getCategory(),
+                            proof.getComment(),
+                            proof.getAvgScore(),
+                            proof.getJudgements()));
+                }
+            }
+        }
+
+
+        return Optional.empty();
     }
 
     public void saveProof(ProofAddDto proofAddDto) {
