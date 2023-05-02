ed617b33754d84fdf5640a37e66d0b9988a84717
blumentopfy <81259038+blumentopfy@users.noreply.github.com>
2023-05-02 20:43:36 +0200

    DTLYD-134 Edited pom Added surefire plugin

--- backend/src/test/java/de/dontletyoudie/backend/CucumberRunnerTest.java
+++ backend/src/test/java/de/dontletyoudie/backend/CucumberRunnerTest.java
@@ -3,8 +3,10 @@ package de.dontletyoudie.backend;
 import io.cucumber.junit.Cucumber;
 import io.cucumber.junit.CucumberOptions;
 import org.junit.runner.RunWith;
+import org.springframework.boot.test.context.SpringBootTest;
 
 @RunWith(Cucumber.class)
+@SpringBootTest
 @CucumberOptions(
         features = {"./src/test/resources/features"},
         plugin = {"pretty"},
