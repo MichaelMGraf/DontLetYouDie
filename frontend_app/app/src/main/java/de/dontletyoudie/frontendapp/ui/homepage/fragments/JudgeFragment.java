5614a36d815d25284d051f9121fd60d4f5baab54
GloriaSelina.Landsberger <gloriaselina.landsberger@dm.de>
2023-05-02 18:20:07 +0200

    DTLYD-140 implemented the displaying of a proof image

--- frontend_app/app/src/main/java/de/dontletyoudie/frontendapp/ui/homepage/fragments/JudgeFragment.java
+++ frontend_app/app/src/main/java/de/dontletyoudie/frontendapp/ui/homepage/fragments/JudgeFragment.java
@@ -1,5 +1,8 @@
 package de.dontletyoudie.frontendapp.ui.homepage.fragments;
 
+import android.graphics.Bitmap;
+import android.graphics.BitmapFactory;
+import android.graphics.drawable.Icon;
 import android.os.Bundle;
 
 import androidx.fragment.app.Fragment;
@@ -12,6 +15,10 @@ import android.widget.ImageView;
 import android.widget.TextView;
 
 import de.dontletyoudie.frontendapp.R;
+import de.dontletyoudie.frontendapp.data.GlobalProperties;
+import de.dontletyoudie.frontendapp.data.apiCalls.CallerStatics;
+import de.dontletyoudie.frontendapp.data.apiCalls.GetProofAPICaller;
+import de.dontletyoudie.frontendapp.data.dto.ProofGetDto;
 
 public class JudgeFragment extends Fragment {
 
@@ -25,7 +32,9 @@ public class JudgeFragment extends Fragment {
     private TextView tv_category;
     private ImageView iv_proof_image;
 
+    private ProofGetDto proof;
 
+    private GetProofAPICaller getProofAPICaller = new GetProofAPICaller(this);
 
     @Override
     public void onCreate(Bundle savedInstanceState) {
@@ -44,6 +53,7 @@ public class JudgeFragment extends Fragment {
         tv_category = view.findViewById(R.id.tv_jugde_category);
         iv_proof_image = view.findViewById(R.id.iv_judge_proof_image);
 
+        getProofAPICaller.executeGET(CallerStatics.APIURL+"api/proof/getPending", GlobalProperties.getInstance().userName);
         button_accept.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
@@ -58,5 +68,16 @@ public class JudgeFragment extends Fragment {
             }
         });
         return view;
+    }
+
+    public void displayProof (ProofGetDto proof) {
+        tv_username.setText(proof.getUsername());
+        tv_category.setText(proof.getCategory());
+        Bitmap bitmap = BitmapFactory.decodeByteArray(proof.getImage(), 0, proof.getImage().length);
+        iv_proof_image.setImageBitmap(bitmap);
+    }
+
+    public void noProofFound () {
+
     }
 }
