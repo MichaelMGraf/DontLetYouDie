b1475fe9d94e54f88a57e3b76735af9acfefba86
blumentopfy <81259038+blumentopfy@users.noreply.github.com>
2023-05-02 23:10:17 +0200

    Make stepdefs extend cucumber runner

--- backend/src/test/java/de/dontletyoudie/backend/cucumberglue/AccountTest.java
+++ backend/src/test/java/de/dontletyoudie/backend/cucumberglue/AccountTest.java
@@ -12,7 +12,7 @@ import de.dontletyoudie.backend.CucumberRunnerTest;
 
 @SpringBootTest(webEnvironment = SpringBootTest.WebEnvironment.RANDOM_PORT)
 @CucumberContextConfiguration
-public class AccountTest {
+public class AccountTest extends CucumberRunnerTest {
 
     @LocalServerPort
     String port;
