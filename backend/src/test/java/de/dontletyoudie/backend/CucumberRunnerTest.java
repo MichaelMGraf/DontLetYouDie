4c43f18821fb7a5c7ff77ab5c983eb1bb5579f64
blumentopfy <81259038+blumentopfy@users.noreply.github.com>
2023-05-02 22:35:02 +0200

    Added further Cucumber Test Runner Configuration

--- backend/src/test/java/de/dontletyoudie/backend/CucumberRunnerTest.java
+++ backend/src/test/java/de/dontletyoudie/backend/CucumberRunnerTest.java
@@ -2,11 +2,19 @@ package de.dontletyoudie.backend;
 
 import io.cucumber.junit.Cucumber;
 import io.cucumber.junit.CucumberOptions;
+import cucumberglue;
 import org.junit.runner.RunWith;
 import org.springframework.boot.test.context.SpringBootTest;
 
 @RunWith(Cucumber.class)
-@SpringBootTest
+@SpringBootTest(classes = {
+        AccountTest.class,
+        Judgement.class,
+        MiniMe.class,
+        Proof.class,
+        Relationship.class,
+        Shop.class
+})
 @CucumberOptions(
         features = {"./src/test/resources/features"},
         plugin = {"pretty"},
