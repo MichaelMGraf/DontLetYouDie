11d5aa4577fbdc73e7e461b99b3e5db96fe0fdb8
blumentopfy <81259038+blumentopfy@users.noreply.github.com>
2023-05-02 20:43:36 +0200

    DTLYD-134 Edited pom Added surefire plugin

--- backend/src/test/java/de/dontletyoudie/backend/cucumberglue/Relationship.java
+++ backend/src/test/java/de/dontletyoudie/backend/cucumberglue/Relationship.java
@@ -1,11 +1,9 @@
 package de.dontletyoudie.backend.cucumberglue;
 
 import io.cucumber.java.en.And;
-import io.cucumber.spring.CucumberContextConfiguration;
 import org.springframework.boot.test.context.SpringBootTest;
 
 @SpringBootTest(webEnvironment = SpringBootTest.WebEnvironment.RANDOM_PORT)
-@CucumberContextConfiguration
 public class Relationship {
     @And("the source account name passed is {string}")
     public void theSourceAccountNamePassedIs(String arg0) {
