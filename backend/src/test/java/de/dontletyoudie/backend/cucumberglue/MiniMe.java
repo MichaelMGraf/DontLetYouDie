5905bb2ba4addb5f375c8f4be5d7282ccd13ba60
blumentopfy <81259038+blumentopfy@users.noreply.github.com>
2023-05-02 22:46:27 +0200

    Added further configuration

--- backend/src/test/java/de/dontletyoudie/backend/cucumberglue/MiniMe.java
+++ backend/src/test/java/de/dontletyoudie/backend/cucumberglue/MiniMe.java
@@ -2,6 +2,11 @@ package de.dontletyoudie.backend.cucumberglue;
 
 import io.cucumber.java.en.And;
 
+import io.cucumber.spring.CucumberContextConfiguration;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest(webEnvironment = SpringBootTest.WebEnvironment.RANDOM_PORT)
+@CucumberContextConfiguration
 public class MiniMe {
     @And("the reponse contains all necessary data")
     public void theReponseContainsAllNecessaryData() {
