ce5b3cc84f675af28b475610fba0230a4679aaea
micha <mimagra@gmx.net>
2023-05-11 09:37:50 +0200

    refactored constructor in JudgmentService

--- backend/src/main/java/de/dontletyoudie/backend/persistence/judgement/JudgementService.java
+++ backend/src/main/java/de/dontletyoudie/backend/persistence/judgement/JudgementService.java
@@ -4,20 +4,13 @@ import de.dontletyoudie.backend.persistence.account.AccountService;
 import de.dontletyoudie.backend.persistence.account.exceptions.AccountNotFoundException;
 import de.dontletyoudie.backend.persistence.judgement.dtos.JudgementDto;
 import lombok.RequiredArgsConstructor;
-import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
 @Service("judgementService")
 @RequiredArgsConstructor
 public class JudgementService {
-    private JudgementRepository judgementRepository;
-    private AccountService accountService;
-
-    @Autowired
-    public JudgementService(JudgementRepository judgementRepository, AccountService accountService) {
-        this.accountService = accountService;
-        this.judgementRepository = judgementRepository;
-    }
+    private final JudgementRepository judgementRepository;
+    private final AccountService accountService;
 
 
     public Judgement saveJudgement(JudgementDto judgementDto) throws AccountNotFoundException {
