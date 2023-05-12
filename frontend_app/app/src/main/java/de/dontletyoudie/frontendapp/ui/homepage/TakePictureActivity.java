c6fc0225741e8507bcde5c817385c66a91561d28
Illumin80 <passrock0305@gmail.com>
2023-05-12 23:06:31 +0200

    DTLYD-155 changed the order of taking picture and selecting category, added comment field

--- frontend_app/app/src/main/java/de/dontletyoudie/frontendapp/ui/homepage/TakePictureActivity.java
+++ frontend_app/app/src/main/java/de/dontletyoudie/frontendapp/ui/homepage/TakePictureActivity.java
@@ -7,31 +7,26 @@ import androidx.appcompat.app.AppCompatActivity;
 import android.Manifest;
 import android.content.ContentResolver;
 import android.content.ContentValues;
-import android.content.Context;
 import android.content.Intent;
 import android.content.pm.PackageManager;
 import android.database.Cursor;
-import android.graphics.Bitmap;
 import android.net.Uri;
 import android.os.Build;
 import android.os.Bundle;
 import android.os.Environment;
 import android.provider.MediaStore;
 import android.provider.OpenableColumns;
-import android.util.Log;
 import android.view.View;
 import android.webkit.MimeTypeMap;
 import android.widget.AdapterView;
 import android.widget.ArrayAdapter;
 import android.widget.Button;
+import android.widget.EditText;
 import android.widget.ImageView;
 import android.widget.Spinner;
 import android.widget.Toast;
 
-import java.io.ByteArrayOutputStream;
 import java.io.File;
-import java.io.FileOutputStream;
-import java.io.IOException;
 
 import de.dontletyoudie.frontendapp.R;
 import de.dontletyoudie.frontendapp.data.GlobalProperties;
@@ -44,9 +39,10 @@ public class TakePictureActivity extends AppCompatActivity implements AdapterVie
     private static final int PERMISSION_CODE = 1000;
     private static final int IMAGE_CAPTURE_CODE = 1001;
 
-    Button takePhotoButton;
+    Button uploadPictureButton;
     ImageView imageView;
     Spinner categorySpinner;
+    EditText commentEditText;
 
     String category;
     Uri image_uri;
@@ -57,30 +53,20 @@ public class TakePictureActivity extends AppCompatActivity implements AdapterVie
         setContentView(R.layout.activity_take_picture);
 
         imageView = findViewById(R.id.image_view);
-        takePhotoButton = findViewById(R.id.capture_image_btn);
+        uploadPictureButton = findViewById(R.id.upload_image_btn);
         categorySpinner = findViewById(R.id.category_spinner);
+        commentEditText = findViewById(R.id.eT_comment);
 
-        //button click
-        takePhotoButton.setOnClickListener(view -> {
-            if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.M) {
-                if (checkSelfPermission(Manifest.permission.CAMERA) == PackageManager.PERMISSION_DENIED ||
-                        //checkSelfPermission(Manifest.permission.WRITE_EXTERNAL_STORAGE) == PackageManager.PERMISSION_DENIED
-                        !Environment.isExternalStorageManager() || true
-                ) {
-                    //permission not enabled, request it
-                    String[] permission = {Manifest.permission.CAMERA, Manifest.permission.MANAGE_EXTERNAL_STORAGE/*Manifest.permission.WRITE_EXTERNAL_STORAGE*/};
-                    requestPermissions(permission, PERMISSION_CODE);
-                    //show popup to request permissions
-                } else {
-                    //permission already granted
-                    openCamera();
-                }
-            }  // system os < marshmallow
+        String[] permission = {Manifest.permission.CAMERA, Manifest.permission.MANAGE_EXTERNAL_STORAGE/*Manifest.permission.WRITE_EXTERNAL_STORAGE*/};
+        requestPermissions(permission, PERMISSION_CODE);
 
+        //button click upload
+         uploadPictureButton.setOnClickListener(view -> {
+             onClickUploadButton();
         });
 
         //for category spinner
-        //state wich string array is to be displayed here
+        //state which string array is to be displayed here
         ArrayAdapter<CharSequence> adapter = ArrayAdapter.createFromResource(this, R.array.category, android.R.layout.simple_spinner_item);
         adapter.setDropDownViewResource(android.R.layout.simple_spinner_dropdown_item);
         categorySpinner.setAdapter(adapter);
@@ -88,19 +74,10 @@ public class TakePictureActivity extends AppCompatActivity implements AdapterVie
         categorySpinner.setOnItemSelectedListener(this);
     }
 
-    private void openCamera() {
-        ContentValues values = new ContentValues();
-        values.put(MediaStore.Images.Media.TITLE, "New Picture");
-        values.put(MediaStore.Images.Media.DESCRIPTION, "From the Camera");
-        image_uri = getContentResolver().insert(MediaStore.Images.Media.EXTERNAL_CONTENT_URI, values);
-        //Camera intent
-        Intent cameraIntent = new Intent(MediaStore.ACTION_IMAGE_CAPTURE);
-        cameraIntent.putExtra(MediaStore.EXTRA_OUTPUT, image_uri);
-        startActivityForResult(cameraIntent, IMAGE_CAPTURE_CODE);
 
-    }
 
-    //handling permission result
+    //handling permission result:
+    //if permission granted: open camera
     @Override
     public void onRequestPermissionsResult(int requestCode, @NonNull String[] permissions, @NonNull int[] grantResults) {
         //this method is called, when user presses Allow or Deny from Permission Request Popup
@@ -116,42 +93,73 @@ public class TakePictureActivity extends AppCompatActivity implements AdapterVie
         }
     }
 
+    // called when image was captured from camera. We just want to set the imageview
     @Override
     protected void onActivityResult(int requestCode, int resultCode, @Nullable Intent data) {
-        // called when image was captured from camera
-
         super.onActivityResult(requestCode, resultCode, data);
         if (resultCode == RESULT_OK) {
             //set the image captured to our Imageview
             imageView.setImageURI(image_uri);
-            UploadPictureAPICaller uploadPictureAPICaller = new UploadPictureAPICaller(this);
-            String username = GlobalProperties.getInstance().userName;
-
-            ContentResolver cR = getApplicationContext().getContentResolver();
-            MimeTypeMap mime = MimeTypeMap.getSingleton();
-            String type = mime.getExtensionFromMimeType(cR.getType(image_uri));
-            String[] segments = image_uri.toString().split("/");
-
-            String attemptGetFileName = queryName(cR, image_uri);
-
-            //create a file to write bitmap data
-            File f = new File("/storage/emulated/0/Pictures/" + attemptGetFileName);
-
-
-            uploadPictureAPICaller.executePOST(new HttpUrl.Builder()
-                            .host(CallerStatics.HOSTIP)
-                            .port(8080)
-                            .addPathSegment("api")
-                            .addPathSegment("proof")
-                            .addPathSegment("add")
-                            .addQueryParameter("username", username)
-                            //TODO Implement querying for comment after taking a picture and replace this or replace comment everywhere
-                            .addQueryParameter("comment", "exampleComment")
-                            .addQueryParameter("category", category)
-                            .scheme("http")
-                            .build(),
-                    f);
         }
+    }
+
+
+
+    //because this class is now implementing OnItemSelectedListener
+    @Override
+    public void onItemSelected(AdapterView<?> parent, View view, int position, long id) {
+        category = parent.getItemAtPosition(position).toString();
+        if(!category.equals("select category")) {
+            uploadPictureButton.setEnabled(true);
+        } else {
+            uploadPictureButton.setEnabled(false);
+        }
+    }
+
+    //because this class is now implementing OnItemSelectedListener
+    @Override
+    public void onNothingSelected(AdapterView<?> parent) {
+        showMessage("please select the fitting category");
+    }
+
+    //opens camera. This happens in a new activity, and onActivityResult gets called automatically when done
+    private void openCamera() {
+        ContentValues values = new ContentValues();
+        values.put(MediaStore.Images.Media.TITLE, "New Picture");
+        values.put(MediaStore.Images.Media.DESCRIPTION, "From the Camera");
+        image_uri = getContentResolver().insert(MediaStore.Images.Media.EXTERNAL_CONTENT_URI, values);
+        //Camera intent
+        Intent cameraIntent = new Intent(MediaStore.ACTION_IMAGE_CAPTURE);
+        cameraIntent.putExtra(MediaStore.EXTRA_OUTPUT, image_uri);
+        startActivityForResult(cameraIntent, IMAGE_CAPTURE_CODE);
+    }
+
+    public void onClickUploadButton(){
+        UploadPictureAPICaller uploadPictureAPICaller = new UploadPictureAPICaller(this);
+        String username = GlobalProperties.getInstance().userName;
+        ContentResolver cR = getApplicationContext().getContentResolver();
+        String attemptGetFileName = queryName(cR, image_uri);
+        String comment = commentEditText.getText().toString();
+
+        //create a file to write bitmap data
+        File f = new File("/storage/emulated/0/Pictures/" + attemptGetFileName);
+
+        uploadPictureAPICaller.executePOST(new HttpUrl.Builder()
+                        .host(CallerStatics.HOSTIP)
+                        .port(8080)
+                        .addPathSegment("api")
+                        .addPathSegment("proof")
+                        .addPathSegment("add")
+                        .addQueryParameter("username", username)
+                        .addQueryParameter("comment", comment)
+                        .addQueryParameter("category", category)
+                        .scheme("http")
+                        .build(),
+                f);
+    }
+
+    public void showMessage(String msg) {
+        Toast.makeText(getApplicationContext(), msg, Toast.LENGTH_LONG).show();
     }
 
     public void navigateToMainActivity() {
@@ -169,26 +177,5 @@ public class TakePictureActivity extends AppCompatActivity implements AdapterVie
         String name = returnCursor.getString(nameIndex);
         returnCursor.close();
         return name;
-    }
-
-    public void showMessage(String msg) {
-        Toast.makeText(getApplicationContext(), msg, Toast.LENGTH_LONG).show();
-    }
-
-    //because this class is now implementing OnItemSelectedListener
-    @Override
-    public void onItemSelected(AdapterView<?> parent, View view, int position, long id) {
-        category = parent.getItemAtPosition(position).toString();
-        if(!category.equals("select category")) {
-            takePhotoButton.setEnabled(true);
-        } else {
-            takePhotoButton.setEnabled(false);
-        }
-    }
-
-    //because this class is now implementing OnItemSelectedListener
-    @Override
-    public void onNothingSelected(AdapterView<?> parent) {
-        showMessage("please select the fitting category");
     }
 }
