be3fc38911d761ad54a65f76383dd1bb2019b7d1
micha <mimagra@gmx.net>
2023-05-13 01:25:18 +0200

    inserted the bulk dummy data from the files at startup

--- backend/src/main/java/de/dontletyoudie/backend/persistence/judgement/dtos/JudgementDto.java
+++ backend/src/main/java/de/dontletyoudie/backend/persistence/judgement/dtos/JudgementDto.java
@@ -3,11 +3,13 @@ package de.dontletyoudie.backend.persistence.judgement.dtos;
 import com.fasterxml.jackson.annotation.JsonProperty;
 import lombok.AllArgsConstructor;
 import lombok.Getter;
+import lombok.NoArgsConstructor;
 import lombok.Setter;
 
 @Getter
 @Setter
 @AllArgsConstructor
+@NoArgsConstructor
 public class JudgementDto {
 
     @JsonProperty
