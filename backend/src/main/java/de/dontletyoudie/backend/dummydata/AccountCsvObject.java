9ee509ed8cccf5e0a13a8409d41e20e808da7237
micha <mimagra@gmx.net>
2023-05-13 01:14:20 +0200

    DTLYD-159 created mapping classes to read the dummy data

---
+++ backend/src/main/java/de/dontletyoudie/backend/dummydata/AccountCsvObject.java
@@ -0,0 +1,9 @@
+package de.dontletyoudie.backend.dummydata;
+
+public class AccountCsvObject {
+
+    public int id;
+    public String benutzername;
+    public String passwort;
+    public String email;
+}
